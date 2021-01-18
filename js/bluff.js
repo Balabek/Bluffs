968a45357950e0a9366b77d4e00d048d6ed7df61
Balabek <vonbabayan@gmail.com>
2021-01-18 21:43:03 +0300

    Fixed the issue of wrong 'go-to' anchor scroll on lower screen widths.

--- js/bluff.js
+++ js/bluff.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
 
-    // Animating the "go-to" anchor scroll
-    // $('a.anchor').click(function(){
-    //     $('html, body').animate({
-    //         scrollTop: $($(this).attr('href') ).offset().top -5
-    //     }, 800);
-    // });
+    // Properly position the "go-to" anchor scroll for lower screen widths
+    if ((screen.width < 991)) {
+        $('a.anchor').click(function(){
+            $('html, body').animate({
+                scrollTop: $($(this).attr('href') ).offset().top -250
+            }, 800);
+        });
+    }
+    else {
+
+        $('a.anchor').click(function(){
+            $('html, body').animate({
+                scrollTop: $($(this).attr('href') ).offset().top -1
+            }, 800);
+        });
+    }
+
 
 
     // Partner brands slider
@@ -161,10 +172,9 @@ $(document).ready(function() {
     });
 
     // Fancy animation for hamburger menu
-    $('.bluffs_nav_toggler').on('click', function () {
+    $('.bluffs_nav_toggler').on('click', function(){
         $('.animated-icon1').toggleClass('open');
     });
-
 
 
 });
